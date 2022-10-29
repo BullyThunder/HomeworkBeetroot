bb3fd357b68f3bbabbedc8fa3cd4dec02612cfa4
Saiko Ruslan <vamosbarca2407@gmail.com>
2022-10-29 20:37:43 +0300

    hw

---
+++ 23_js_homework/second_part/js/main.js
@@ -0,0 +1,12 @@
+// 1 завдання
+let name = prompt("Яке в тебе ім'я?");
+console.log("Привіт, " + name);
+// 2 завдання
+let year = prompt("Який ваш рік народження", 0);
+const now_year = 2022;
+console.log("Вам зараз " + (now_year - year));
+// 3 завдання
+let side = prompt("Введіть довжину сторони квадрату", 0);
+let perimeter = side * 4;
+console.log("Периметр квадрату дорівнює = " + perimeter);
+
---
+++ 23_js_homework/third_part/js/main.js
@@ -0,0 +1,15 @@
+// 1 Завдання
+let radius = prompt("Введіть радіус кола", 0);
+const numberP = 3.14;
+let square = numberP * radius * 2;
+console.log("Площа круга дорівнює " + square);
+// 2 Завдання
+let distance = prompt("Які відстань між двома пунктами ", 0);
+let time = prompt("За скільки годин вам потрібно добратися", 0);
+let speed = distance / time;
+console.log("Вам потрібно їхати зі швидкістю " + speed + " км/г");
+// 3 Завдання
+let usd = prompt("Введіть кількість долларів", 0);
+const Exchange = 36.94;
+let uah = usd * Exchange;
+console.log("Ми дамо вам " + Math.round(uah * 100)/100 + " грн");
